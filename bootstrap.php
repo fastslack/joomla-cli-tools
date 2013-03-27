f81511f1d12453a41d001e6946302d1f19ce2419
Matias Aguirre <maguirre@matware.com.ar>
2013-03-27 15:33:35 -0300

    Adding bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @version		    $Id: 
+ * @package		    Matware
+ * @subpackage	  JoomlaCliTools
+ * @copyright			CopyRight 2004-2013 Matware All rights reserved.
+ * @author				Matias Aguirre
+ * @email   			maguirre@matware.com.ar
+ * @link					http://www.matware.com.ar/
+ * @license				GNU/GPL http://www.gnu.org/licenses/gpl-2.0-standalone.html
+ */
+
+// Setup the base path related constant.
+//define('JPATH_LIBRARIES', dirname(dirname(__FILE__)).'/joomla-cms.my/libraries'   );
+
+// Import the Joomla! CMS Libraries
+//require JPATH_LIBRARIES.'/import.legacy.php';
+require dirname(dirname(__FILE__)).'/joomla-platform.my/libraries/import.php';
