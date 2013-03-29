c0ad86115e33e09d02ffca18ed8cbc2925417132
Matias Aguirre <maguirre@matware.com.ar>
2013-03-29 12:19:17 -0300

    Using joomla-framework instead joomla-platform

--- bootstrap.php
+++ bootstrap.php
@@ -4,12 +4,12 @@
 *
 * @version $Id$
 * @package MatWare
-*	@subpackage JoomlaCliTools
-*	@copyright Copyright 2004 - 2013 Matias Aguire. All rights reserved.
+* @subpackage JoomlaCliTools
+* @copyright Copyright 2004 - 2013 Matias Aguire. All rights reserved.
 * @license GNU General Public License version 2 or later.
 * @author Matias Aguirre <maguirre@matware.com.ar>
 * @link http://www.matware.com.ar
 */
 
 // Import the Joomla! CMS Libraries
-require dirname(dirname(__FILE__)).'/joomla-platform/libraries/import.php';
+require dirname(dirname(__FILE__)).'/joomla-framework/libraries/import.php';
