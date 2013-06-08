4f29b8186ee4e593044ae9ef27ceed6bef52f63c
fastslack <maguirre@matware.com.ar>
2013-06-08 12:07:53 -0300

    Update bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -5,14 +5,14 @@
 * @version $Id$
 * @package MatWare
 * @subpackage JoomlaCliTools
-* @copyright Copyright 2004 - 2013 Matias Aguire. All rights reserved.
+* @copyright Copyright 2004 - 2013 Matias Aguirre. All rights reserved.
 * @license GNU General Public License version 2 or later.
 * @author Matias Aguirre <maguirre@matware.com.ar>
 * @link http://www.matware.com.ar
 */
 
 // Import the Joomla! CMS Libraries
-require dirname(dirname(__FILE__)).'/joomla-platform/libraries/import.php';
+require dirname(dirname(__FILE__)).'/joomla-cms/libraries/import.php';
 // Import the database libraries
 jimport('joomla.database.database');
 // Import the file libraries
