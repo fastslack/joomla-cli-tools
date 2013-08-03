3597b8aabdad1f34e135cfb45e45e726aa7f952a
Matias Aguirre <maguirre@matware.com.ar>
2013-08-03 19:12:44 -0300

    Update bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -12,8 +12,39 @@
 */
 
 // Import the Joomla! CMS Libraries
-require dirname(dirname(__FILE__)).'/joomla-cms/libraries/import.php';
+//require_once dirname(dirname(__FILE__)).'/joomla-cms.rest/libraries/import.php';
+
+// Look for the Joomla! root path.
+$ROOT = dirname(dirname(__FILE__)).'/joomla-cms.rest';
+
+// Ensure that required path constants are defined.
+if (!defined('JPATH_LIBRARIES'))
+{
+	define('JPATH_LIBRARIES', $ROOT.'/libraries');
+}
+// Fire up the Platform importer.
+if (file_exists(JPATH_LIBRARIES . '/import.php'))
+{
+	require JPATH_LIBRARIES . '/import.php';
+}
+// Ensure that required path constants are defined.
+if (!defined('JPATH_SITE'))
+{
+	define('JPATH_SITE', $ROOT);
+}
+
 // Import the database libraries
 jimport('joomla.database.database');
 // Import the file libraries
 jimport('joomla.filesystem.file');
+// Import the html libraries
+jimport('cms.html.html');
+// Load redRAD if it exists
+$redradLoader = JPATH_PLATFORM . '/redrad/bootstrap.php';
+
+if (file_exists($redradLoader))
+{
+	require_once $redradLoader;
+
+	JLoader::registerPrefix('J',  JPATH_LIBRARIES . '/redrad/joomla');
+}
