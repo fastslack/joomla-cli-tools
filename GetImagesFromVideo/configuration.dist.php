ebf1ea52a55d665ad8c062db7c5261366a1ea4f0
Matias Aguirre <maguirre@matware.com.ar>
2015-05-19 01:39:43 -0300

    GetImagesFromVideo update

--- GetImagesFromVideo/configuration.dist.php
+++ GetImagesFromVideo/configuration.dist.php
@@ -4,8 +4,8 @@
 *
 * @version $Id$
 * @package JoomlaCliTools
-* @subpackage CreateTableClass
-* @copyright Copyright 2004 - 2013 Matias Aguire. All rights reserved.
+* @subpackage GetImagesFromVideo
+* @copyright Copyright 2004 - 2015 Matias Aguire. All rights reserved.
 * @license GNU General Public License version 2 or later.
 * @author Matias Aguirre <maguirre@matware.com.ar>
 * @link http://www.matware.com.ar
@@ -15,7 +15,7 @@
 defined('_JEXEC') or die;
 
 /**
-* CreateTableClass configuration class.
+* GetImagesFromVideo configuration class.
 *
 * @package JoomlaCliTools
 * @since 1.0
@@ -28,13 +28,5 @@ final class JConfig
 	* @var string
 	* @since 1.0
 	*/
-	public $dbtype = 'mysqli';
-	public $host = 'localhost';
-	public $user = '';
-	public $password = '';
-	public $db = '';
-	public $dbprefix = 'jos_';
-
-	public $classname = 'JCliToolsTableExample';
-	public $table = '#__categories';
+	public $path = './videos';
 }
